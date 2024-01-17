40978f77f02c0ee691795ad034a9def1bc5b8ca0
NTNhaan <thanhnhannguyen30082003@gmail.com>
2024-01-17 21:05:14 +0700

    create new branch and add file test

--- src/Styles/TestScrapingWeb.js
+++ src/Styles/TestScrapingWeb.js
@@ -1,44 +1,106 @@
+// import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import cheerio from "react-native-cheerio/lib/cheerio";
+// import { index } from "cheerio/lib/api/traversing";
+// const url = "https://dkmh.tdmu.edu.vn/#/tkb-tuan";
+
+// async function getHTML() {
+//   const axiosConfig = {
+//     headers: {
+//       // user agent này sẽ trả về một chuỗi ký tự mà trình duyệt gửi đến máy chủ. Khắc phục lỗi 403 khi truy cập trang web
+//       "User-Agent":
+//         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+//     },
+//   };
+//   const { data: html } = await axios.get(url, axiosConfig);
+//   return html;
+// }
+
+// const TestScrapingWeb = () => {
+//   const [movieData, setMovieData] = useState([]);
+
+//   const handleScrapeData = async () => {
+//     try {
+//       const res = await getHTML();
+//       const $ = cheerio.load(res);
+//       const movieData = [];
+
+//       $(".table-responsive-sm .ng-star-inserted p").each((i, movie) => {
+//         const title = $(movie).text().trim();
+//         // const rating = $(movie).find(".ratingColumn strong").text();
+//         movieData.push(title);
+//       });
+
+//       setMovieData(movieData);
+//       // await RNFS.writeFile("moviedata.json", JSON.stringify(movieData));
+//       console.log("Scraping data: ", movieData);
+//     } catch (error) {
+//       console.error("Error scraping data:", error);
+//     }
+//   };
+//   return (
+//     <View>
+//       <TouchableOpacity onPress={handleScrapeData}>
+//         <View>
+//           <Text>Start Scraping Data From Website</Text>
+//         </View>
+//       </TouchableOpacity>
+//       {movieData.map((movie, index) => (
+//         <Text key={index}>{movie}</Text>
+//       ))}
+//     </View>
+//   );
+// };
+
+// export default TestScrapingWeb;
+
+// const styles = StyleSheet.create({});
+
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import cheerio from "react-native-cheerio/lib/cheerio";
+import { index } from "cheerio/lib/api/traversing";
+const url = "https://dkmh.tdmu.edu.vn/#/tkb-tuan/";
 
-const url = "https://www.imdb.com/chart/top/";
-const movieData = {};
+// const loginAndGetCookies = async () => {
+//   try {
+//     const loginRes = await axios.post("https://dkmh.tdmu.edu.vn/#/tkb-tuan/", {
+//       username: "2124802010059@student.tdmu.edu.vn",
+//       password: "Thanhnhan20032@",
+//     });
+//     const cookies = loginRes.headers["set-cookie"];
+//     return cookies;
+//   } catch (error) {
+//     console.error("Error logging in:", error);
+//     throw error;
+//   }
+// };
 
-async function getHTML() {
-  const axiosConfig = {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-    },
-  };
-  const { data: html } = await axios.get(url, axiosConfig);
-  return html;
-}
-
-// getHTML().then((res) => {
-//   const $ = cheerio.load(res);
-//   $(".lister-list>tr").each((i, movie) => {
-//     const title = $(movie).find(".titleColumn a").text();
-//     const rating = $(movie).find(".ratingColumn strong").text();
-//     movieData[title] = rating;
-//   });
-//   RNFS.writeFile("moviedata.json", JSON.stringify(movieData), (err) => {
-//     if (err) throw err;
-//     console.log("file successfully saved!");
-//   });
-// });
 const TestScrapingWeb = () => {
+  const [movieData, setMovieData] = useState([]);
+
   const handleScrapeData = async () => {
     try {
-      const res = await getHTML();
-      const $ = cheerio.load(res);
-      $(".lister-list>tr").each((i, movie) => {
-        const title = $(movie).find(".titleColumn a").text();
-        const rating = $(movie).find(".ratingColumn strong").text();
-        movieData[title] = rating;
+      // const cookies = await loginAndGetCookies();
+
+      const res = await axios.get(url, {
+        headers: {
+          // Cookie: cookies,
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+        },
       });
+      const $ = cheerio.load(res.data);
+      const movieData = [];
+
+      $(".table-responsive-sm .ng-star-inserted p").each((i, movie) => {
+        const title = $(movie).text().trim();
+        movieData.push(title);
+      });
+
+      setMovieData(movieData);
       // await RNFS.writeFile("moviedata.json", JSON.stringify(movieData));
       console.log("Scraping data: ", movieData);
     } catch (error) {
@@ -52,6 +114,9 @@ const TestScrapingWeb = () => {
           <Text>Start Scraping Data From Website</Text>
         </View>
       </TouchableOpacity>
+      {movieData.map((movie, index) => (
+        <Text key={index}>{movie}</Text>
+      ))}
     </View>
   );
 };
---
+++ src/Styles/test.js
@@ -0,0 +1,12 @@
+<ul class="ipc-metadata-list ipc-metadata-list--dividers-between sc-71ed9118-0 kxsUNk compact-list-view ipc-metadata-list--base">
+  <li class="ipc-metadata-list-summary-item sc-3f724978-0 enKyEL cli-parent">
+    <div class="ipc-metadata-list-summary-item__c">
+      <div class="ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-43986a27-9 gaoUku cli-title">
+        <a class="ipc-title-link-wrapper">
+          <h3 class="ipc-title__text">1. The Shawshank Redemption</h3>
+        </a>
+      </div>
+    </div>
+  </li>
+  <li></li>
+</ul>;
